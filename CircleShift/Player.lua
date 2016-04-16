e3b6b69862d27ba2cfef12cf2200347650dbc59d
weakman54 <enkiigm@gmail.com>
2016-04-16 23:34:48 +0200

    Added simple tween "straight-over"

--- CircleShift/Player.lua
+++ CircleShift/Player.lua
@@ -12,12 +12,38 @@ function player:update(dt)
     
         if input:pressed("movement") then
             print("Move")
-            --self.moving = true
+            
+            self.moving = true
     
             local nextDiscI = ((self.engagedTo.index) % #self.engagedTo.parent.discs) + 1 -- Uuh, this seems pretty wierd... oh well..
             local nextDisc =  self.engagedTo.parent.discs[nextDiscI] -- yeah...
-    
-            self.engagedTo = nextDisc
+
+            player.engagedTo = false
+
+            -- Using my own function since tween loves to move the disc together with
+            -- the player...
+            local startPos  = player.pos:clone() -- I think I need a clone here?
+            local targetPos = nextDisc.center
+            local targetV =   targetPos - startPos
+            
+            local t = 0
+            local duration = 1
+            
+            Timer.during(duration, function(dt)
+                t = t + dt
+                local fraction = t/duration
+
+                player.pos = -targetV * fraction*(fraction-2) + startPos
+            end, function()
+                player.moving = false
+                player.engagedTo = nextDisc
+            end)
+
+            -- OK, WTF!? Keeping this here for posterity...
+            --Timer.tween(duration, player.pos, targetV, "out-quad", function()
+             --   player.moving = false
+              --  player.engagedTo = nextDisc
+            --end)
         end
     end
 end
