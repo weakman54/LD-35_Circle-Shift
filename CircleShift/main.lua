e6c68c9bdf595e6717ee0059f1cfca43066f0b3f
weakman54 <enkiigm@gmail.com>
2016-04-16 14:43:52 +0200

    Made a simple love framework

---
+++ CircleShift/main.lua
@@ -0,0 +1,20 @@
+--[[
+Circle Shift, a simple Lua/Löve2d game made for Ludum Dare 35
+made by weakman54
+]]--
+
+
+
+function love.load()
+    
+end
+
+
+function love.update(dt)
+    
+end
+
+
+function love.draw()
+    love.graphics.print("Hello World", 400, 300)
+end
